2179b3ee9caa16ecd02e4d764e5158ae9eca273e
Ajani Bilby <aj@bilby.net>
2019-02-11 18:46:30 +1100

    +Lexer: Rule matching

--- transpiler/interpreter/lexer.js
+++ transpiler/interpreter/lexer.js
@@ -1,8 +1,26 @@
+const path = require('path');
 const fs = require('fs');
+
+const Pattern = require('./pattern.js');
+
+
+
+/*-----------------------------------------------
+	Generate the lexer tree
+-----------------------------------------------*/
+let pattern = [];
+let raw = JSON.parse( fs.readFileSync(path.join(__dirname, './structure.json'), "utf8") );
+for (let key in raw.pattern){
+	raw.pattern[key].name = key;
+	pattern.push(new Pattern(pattern, raw.pattern[key]));
+}
+
+
 
 
 function Process(text){
 	let structure = {};
+	text = text.replace(/\n\r/g, '\n');
 
 
 	return structure;
